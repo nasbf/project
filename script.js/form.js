cccefc958fd6fd8ecfec4a6f409a741fd7ef86ad
nasbf <mo0ma@hotmail.com>
2024-12-14 23:04:41 -0500

    update

---
+++ script.js/form.js
@@ -0,0 +1,42 @@
+function calculateBudget() {
+    const numGests = parseInt(document.getElementById("pax").value, 10);
+    const valorstate = parseInt(document.getElementById("valorstate").value, 10);
+    console.log(numGests);
+
+    let totalGuests;
+    if (numGests === 50) {
+        document.getElementById("valor").value = numGests * 228000;
+        document.getElementById("totalevent").value = valorstate + (numGests * 228000);
+
+    } else if (numGests === 80) {
+        document.getElementById("valor").value = numGests * 185000;
+        document.getElementById("totalevent").value = valorstate + (numGests * 185000);
+
+    } else if (numGests === 100) {
+        document.getElementById("valor").value = numGests * 167000;
+        document.getElementById("totalevent").value = valorstate + (numGests * 167000);
+
+    } else if (numGests === 200){
+        document.getElementById("valor").value = numGests * 132000;
+        document.getElementById("totalevent").value = valorstate + (numGests * 132000);
+    }
+
+}
+
+
+
+
+function sendInfo(event) {
+    event.preventDefault();
+    const formulary = document.getElementById("formulary");
+    const result = document.getElementById("result");
+    // const totalbudget = calculateBudget();
+    // const result = document.getElementById("value");
+    // result.innerText = `$${totalBudget.toLocaleString()}`;
+    formulary.style.display = "none";
+    result.style.display = "block";
+    calculateBudget();
+};
+
+
+console.log(totalGuests);
---
+++ script.js/home.js
@@ -0,0 +1,106 @@
+
+const menu = document.querySelector("#menu")
+const hambutton = document.querySelector('nav');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+const photos = [
+    {
+        id: "pozo claro",
+        imgSrc: "images/pozoclaroexterior.webp"
+    },
+    {
+        id: "pozo claro",
+        imgSrc: "images/pozoclarocapilla2.webp"
+    },
+    {
+        id: "pozo claro",
+        imgSrc: "images/capilla_pozoclaro-L.jpg"
+    },
+    {
+        id: "pozo claro",
+        imgSrc: "images/pozoclarobosque3.webp"
+    },
+    {
+        id: "remanso",
+        imgSrc: "images/capilla_remanso.webp"
+    },
+    {
+        id: "nogales",
+        imgSrc: "images/salon_los_nogales.webp"
+    }
+]
+
+function photo(items) {
+    return `<figure>
+                <img src="${items.imgSrc}" alt=${items.id} loading = "lazy">
+                <figcaption>
+                    <h3> Country State ${items.id}</h3>
+                </figcaption>
+            </figure>`;
+
+}
+
+
+function renderitems(photos) {
+    const html = photos.map(photo);
+    document.querySelector(".gallery").innerHTML = html.join(" ");
+    // const pozoclaro = photos.filter(photo => photo.id === "pozo claro");
+    //     renderitems (pozoclaro);
+}
+
+const first = document.getElementById("home");
+first.addEventListener(`click`, () => {
+    const onlypozoclaro = photos.filter(photo => photo.id === "pozoclaro");
+    renderitems(onlypozoclaro);
+});
+
+
+const second = document.getElementById("more");
+second.addEventListener(`click`, () => {
+    function desaperSection() {
+        return `<section>
+                    <h3>Another Fantastic States</h3>
+                <p>Gourmet Real counts with other locations according your neededs </p>
+                </section>`;
+
+    }
+
+    const heroElement = document.querySelector("#welcome");
+    heroElement.innerHTML = desaperSection();
+
+    const imagenElement = document.querySelector("#firstimagen");
+    imagenElement.style.display = "none";
+
+    const diferentPozoClaro = photos.filter(photo => photo.id !== "pozo claro");
+    renderitems(diferentPozoClaro);
+
+
+});
+
+
+
+const galleryTotal = document.getElementById("gallery");
+galleryTotal.addEventListener(`click`, () => {
+    renderitems(photos);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
